761106ad229b47e272d6b773a3525175c2cca1c4
Alexander Craggs <eyrecraggs@gmail.com>
2019-07-14 20:20:29 +0100

    first commit

---
+++ spec/eyre-c-snippets-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import EyreCSnippetsView from '../lib/eyre-c-snippets-view';
+
+describe('EyreCSnippetsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
